cdd00e086a30044cea448966777aeaf2faf9170e
Adam Aske <adamaske@live.no>
2021-04-26 14:45:12 +0200

    test

--- Group9/Source/Group9/BipedEnemy.cpp
+++ Group9/Source/Group9/BipedEnemy.cpp
@@ -2,6 +2,7 @@
 
 
 #include "BipedEnemy.h"
+<<<<<<< HEAD
 #include "GameFramework/Controller.h"
 
 
@@ -16,6 +17,12 @@ void ABipedEnemy::BeginPlay()
 	Super::BeginPlay();
 
 	//AIController = Cast<AAIController>(GetController());
+=======
+#include "GameFramework/CharacterMovementComponent.h"
+#include "AIController.h"
+#include "Kismet/KismetSystemLibrary.h"//GetController
+
+>>>>>>> parent of fae415e (sad)
 
 	SphereTrigger->OnComponentBeginOverlap.AddDynamic(this, &ABipedEnemy::OnOverlap);
 	SphereTrigger->OnComponentEndOverlap.AddDynamic(this, &ABipedEnemy::OnOverlapEnd);
@@ -36,7 +43,18 @@ ABipedEnemy::ABipedEnemy()
 	SphereTrigger = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Trigger"));
 	SphereTrigger->SetupAttachment(RootComponent);
 	SphereTrigger->InitSphereRadius(600.f);
+<<<<<<< HEAD
 
+=======
+
+
+
+}
+
+void ABipedEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+>>>>>>> parent of fae415e (sad)
 
 
 }
@@ -58,12 +76,15 @@ void ABipedEnemy::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Ot
 
 void ABipedEnemy::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex)
 {
+<<<<<<< HEAD
 	APlayerUnit* Player = Cast<APlayerUnit>(OtherActor);
 	if (AIController && Player)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("out of unit area"));
 		AIController->StopMovement();
 	}
+=======
+>>>>>>> parent of fae415e (sad)
 }
 
 void  ABipedEnemy::MoveToTarget(APlayerUnit* PlayerUnit1)
@@ -75,6 +96,7 @@ void  ABipedEnemy::MoveToTarget(APlayerUnit* PlayerUnit1)
 		FAIMoveRequest AIMoverequest;
 		AIMoverequest.SetGoalActor(PlayerUnit);
 		AIMoverequest.SetAcceptanceRadius(25.f);
+<<<<<<< HEAD
 
 		//creates a nav path
 		FNavPathSharedPtr NavPtr;
@@ -82,5 +104,7 @@ void  ABipedEnemy::MoveToTarget(APlayerUnit* PlayerUnit1)
 		AIController->MoveTo(AIMoverequest, &NavPtr);
 
 
+=======
+>>>>>>> parent of fae415e (sad)
 	}
 }
