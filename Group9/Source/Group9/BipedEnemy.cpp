beab25a8ba3d9d9081c72a157ad7cc5904e9712e
Meijkiqman <70415668+Meijkiqman@users.noreply.github.com>
2021-05-17 15:25:58 +0200

    gfg

--- Group9/Source/Group9/BipedEnemy.cpp
+++ Group9/Source/Group9/BipedEnemy.cpp
@@ -2,46 +2,139 @@
 
 
 #include "BipedEnemy.h"
-#include "GameFramework/Controller.h"
-#include "GameFramework/CharacterMovementComponent.h"
 #include "AIController.h"
+#include "EnemyUnit.h"
+#include "PlayerUnit.h"
+#include "Components/PrimitiveComponent.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 
 
+ABipedEnemy::ABipedEnemy()
+{
+	RootComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RootComp"));
 
+	BipedMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Biped Mesh"));
+	BipedMesh->SetupAttachment(RootComponent);
 
-
+}
 
 void ABipedEnemy::BeginPlay()
 {
-	//Super::BeginPlay();
+	Super::BeginPlay();
 
-	//AIController = Cast<AAIController>(GetController());
-//GetController
+	PlayerUnit = Cast<APlayerUnit>(UGameplayStatics::GetPlayerCharacter(this, 0));
 }
 
-ABipedEnemy::ABipedEnemy()
+void  ABipedEnemy::MoveUnit(FVector LookAtTarget)
 {
-	//RootComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RootComp"));
-	
-	//BipedMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Biped Mesh"));
-	//BipedMesh->SetupAttachment(RootComponent);
+	//rotates to face player
+	FVector FaceTarget = FVector(LookAtTarget.X, LookAtTarget.Y, BipedMesh->GetComponentLocation().Z);
+	FVector StartLocation = BipedMesh->GetComponentLocation();
 
+	FRotator RotateBiped = FVector(FaceTarget - StartLocation).Rotation();
+	BipedMesh->SetWorldRotation(RotateBiped);
 
+	//move to player
+	LookAtTarget.Z = GetActorLocation().Z;
+	FVector NewDirection = LookAtTarget - GetActorLocation();
+	NewDirection.Normalize();
 
+	SetActorRotation(NewDirection.Rotation());
+
+	FVector MoveVector = GetActorLocation() + GetActorForwardVector() * movementSpeed * 0.1;
+	SetActorLocation(MoveVector);
 
 }
 
-void ABipedEnemy::MoveUnit(FVector LookAtTarget)
+void ABipedEnemy::CloseMeleeAttack(float DeltaTime)
 {
+	//finds how much has gone to determine the attack rate
+	if (CurrentTime >= MeleeHitTime)
+	{
+		//resets melee attack timer after its done 1 cycle
+		//disables melee box collider if overlap
+		if (MeleeBox->GetGenerateOverlapEvents() == true)
+		{
+			MeleeBox->SetGenerateOverlapEvents(false);
+		}
+		//resets melee attack
+		bMeleeHit = false;
+		bIsAttacking = false;
+		CurrentTime = 0;
+	}
+	else
+	{//ignore if enemy has allready struck player
+		if (!bMeleeHit)
+		{//if the timer is outside the set attack cycle, then dont check overlap
+			if (CurrentTime < MeleeStart || CurrentTime >= MeleeEnd)
+			{	//deactive collison box
+				if (MeleeBox->GetGenerateOverlapEvents() == true)
+				{
+					MeleeBox->SetGenerateOverlapEvents(false);
+				}
+			}
+			//if the timer is inside the melee cycle then check for overlap
+			if (CurrentTime > MeleeStart && CurrentTime < MeleeEnd)
+			{
+				if (MeleeBox->GetGenerateOverlapEvents() == false)
+				{
+					MeleeBox->SetGenerateOverlapEvents(true);
+				}
+			}
+		}
+		else
+		{//if melee attack has occured in current cycle, disable box to prevent damage stacking
+			if (MeleeBox->GetGenerateOverlapEvents() == true)
+			{
+				MeleeBox->SetGenerateOverlapEvents(false);
+			}
+		}
+		//adds deltatime to timer for melee cycle
+		CurrentTime += DeltaTime;
+	}
+}
+
+void ABipedEnemy::MeleeHit(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//prevents hit stacking
+	if (bMeleeHit)
+	{
+		return;
+	}
+	//checks if hit overlaps with playerUnit
+	if (OtherActor->IsA(APlayerUnit::StaticClass()))
+	{
+		bMeleeHit = true;
+		UE_LOG(LogTemp, Log, TEXT("Melee Attack Hit Player"))
+			//casts to plyer unit
+			APlayerUnit* playerUnit = Cast<APlayerUnit>(OtherActor);
+		playerUnit->TakeDamage(MeleeDamage);
+	}
 }
 
 void ABipedEnemy::Tick(float DeltaTime)
 {
+	Super::Tick(DeltaTime);
+
+	//Gets location of player and biped to measure the distance between them
+	FVector currentPlayerLocal = PlayerUnit->GetActorLocation();
+	FVector currentBipedLocal = BipedMesh->GetComponentLocation();
+	float MeleeRange = FVector::Distance(currentPlayerLocal, currentBipedLocal);
+
+
+	// checks if playerunit exist to avoid hard crash, and player distance
+	if (!PlayerUnit || BipedStopRange < MeleeRange)
+	{
+		MoveUnit(PlayerUnit->GetActorLocation());
+
+	}
+
+
+
 }
-
-
 float ABipedEnemy::PlayerDistance()
 {
 	return 0.0f;
